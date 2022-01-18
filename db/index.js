16cbdb03f99b72bdea15361364ae476957b9baad
EzyhB <ezyhbmail@gmail.com>
2022-01-18 14:24:12 +0000

    create event table and dummy data

---
+++ db/index.js
@@ -0,0 +1,16 @@
+import pg from "pg";
+import { database } from "pg/lib/defaults";
+
+const env = process.env;
+
+const pool = new pg.Pool({
+  user: env.PG_USER,
+  host: env.PG_HOST,
+  database: env.PG_DATABSE,
+  password: env.PG_PASSWORD,
+  port: env.PG_PORT,
+});
+
+export default function query(text, params) {
+  return pool.query(text, params);
+}
---
+++ db/scripts/dummyData.js
@@ -0,0 +1,10 @@
+const dummyData = [
+  {
+    name: "super event",
+    description: "epic event and stuff",
+    date: "2022-01-18",
+    start: "13:00",
+    duration: "30",
+    category: "Class Schedule",
+  },
+];
