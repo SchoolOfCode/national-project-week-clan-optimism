3c1f09cabf011ce3c6b7ea883a67afe31f1e2243
Dovydas Lozys <reduktorius@gmail.com>
2022-01-20 11:07:17 +0000

    deployment fixes

--- src/components/DateAndTime/index.js
+++ src/components/DateAndTime/index.js
@@ -2,22 +2,16 @@ import React, { useState, useEffect } from "react";
 
 export default function DateAndTime() {
   const [dateTime, setDateTime] = useState({});
-  var today = new Date();
-  var date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
-  useEffect(() => {
-    var timer = setInterval(() => {
-      today = new Date();
-      var time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      var dateTimeObj = { date: date, time: time };
+  setInterval(() => {
+    const today = new Date();
+    const date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const dateTimeObj = { date: date, time: time };
 
-      setDateTime(dateTimeObj);
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
-
+    setDateTime(dateTimeObj);
+  }, 1000);
+  
   return (
     <section>
       <h4>
