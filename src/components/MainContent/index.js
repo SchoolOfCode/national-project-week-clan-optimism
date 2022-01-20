9d60f092a0324c73acbedefedd5dffeb90dc6f77
Tiger Fingers <loreen@anet360.com>
2022-01-20 14:28:55 +0000

    csshomepage edits done

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -9,16 +9,15 @@ export default function MainContent() {
   return (
     <section className="main-display">
       <h1>Clan Optimist's Event App</h1>
-      <h2>next event</h2>
+      <h2>Next Event</h2>
       <DateAndTime />
       <section className="calendar-placement">
         <MyCalendar />
       </section>
       <p>
-        The reminder/calendar solution you will absolutely love. Bootcamp is no
-        walk in the park. So let’s make it a little easier for you. Want to
-        create an event to get together with your fellow bootcampers? Or forgot
-        when code club is? You need the Optimistic event reminder!
+        Bootcamp is no walk in the park. So let’s make it a little easier for you. Want to
+        create an event to get together with your fellow bootcampers? Forgot
+        when code club is? Let's help you!
       </p>
     </section>
   );
