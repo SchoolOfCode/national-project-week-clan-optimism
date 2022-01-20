2098975fbca408164f2ca0bdaa0d6bedf01e507f
EzyhB <ezyhbmail@gmail.com>
2022-01-20 22:26:48 +0000

    css work done, all left is the edit button hiding/displaying propperly

--- src/components/HostEventForm/index.js
+++ src/components/HostEventForm/index.js
@@ -77,27 +77,27 @@ export default function HostEventForm() {
             description="start-time"
             className="input-medium validate-time"
           >
-            <option value="07:00:00">07:00</option>
-            <option value="07:30:00">07:30</option>
-            <option value="08:00:00">08:00</option>
-            <option value="08:30:00">08:30</option>
-            <option value="09:00:00">09:00</option>
-            <option value="09:30:00">09:30</option>
-            <option value="10:00:00">10:00</option>
-            <option value="10:30:00">10:30</option>
-            <option value="11:00:00">11:00</option>
-            <option value="11:30:00">11:30</option>
-            <option value="12:00:00">12:00</option>
-            <option value="12:30:00">12:30</option>
-            <option value="13:00:00">13:00</option>
-            <option value="13:30:00">13:30</option>
-            <option value="14:00:00">14:00</option>
-            <option value="14:30:00">14:30</option>
-            <option value="15:00:00">15:00</option>
-            <option value="15:30:00">15:30</option>
-            <option value="16:00:00">16:00</option>
-            <option value="16:30:00">16:30</option>
-            <option value="17:00:00">17:00</option>
+            <option value="07:00:00">07:00 Start</option>
+            <option value="07:30:00">07:30 Start</option>
+            <option value="08:00:00">08:00 Start</option>
+            <option value="08:30:00">08:30 Start</option>
+            <option value="09:00:00">09:00 Start</option>
+            <option value="09:30:00">09:30 Start</option>
+            <option value="10:00:00">10:00 Start</option>
+            <option value="10:30:00">10:30 Start</option>
+            <option value="11:00:00">11:00 Start</option>
+            <option value="11:30:00">11:30 Start</option>
+            <option value="12:00:00">12:00 Start</option>
+            <option value="12:30:00">12:30 Start</option>
+            <option value="13:00:00">13:00 Start</option>
+            <option value="13:30:00">13:30 Start</option>
+            <option value="14:00:00">14:00 Start</option>
+            <option value="14:30:00">14:30 Start</option>
+            <option value="15:00:00">15:00 Start</option>
+            <option value="15:30:00">15:30 Start</option>
+            <option value="16:00:00">16:00 Start</option>
+            <option value="16:30:00">16:30 Start</option>
+            <option value="17:00:00">17:00 Start</option>
           </select>
         </label>
 
@@ -116,27 +116,27 @@ export default function HostEventForm() {
             description="end-time"
             className="input-medium validate-time"
           >
-            <option value="07:00:00">07:00</option>
-            <option value="07:30:00">07:30</option>
-            <option value="08:00:00">08:00</option>
-            <option value="08:30:00">08:30</option>
-            <option value="09:00:00">09:00</option>
-            <option value="09:30:00">09:30</option>
-            <option value="10:00:00">10:00</option>
-            <option value="10:30:00">10:30</option>
-            <option value="11:00:00">11:00</option>
-            <option value="11:30:00">11:30</option>
-            <option value="12:00:00">12:00</option>
-            <option value="12:30:00">12:30</option>
-            <option value="13:00:00">13:00</option>
-            <option value="13:30:00">13:30</option>
-            <option value="14:00:00">14:00</option>
-            <option value="14:30:00">14:30</option>
-            <option value="15:00:00">15:00</option>
-            <option value="15:30:00">15:30</option>
-            <option value="16:00:00">16:00</option>
-            <option value="16:30:00">16:30</option>
-            <option value="17:00:00">17:00</option>
+            <option value="07:00:00">07:00 End</option>
+            <option value="07:30:00">07:30 End</option>
+            <option value="08:00:00">08:00 End</option>
+            <option value="08:30:00">08:30 End</option>
+            <option value="09:00:00">09:00 End</option>
+            <option value="09:30:00">09:30 End</option>
+            <option value="10:00:00">10:00 End</option>
+            <option value="10:30:00">10:30 End</option>
+            <option value="11:00:00">11:00 End</option>
+            <option value="11:30:00">11:30 End</option>
+            <option value="12:00:00">12:00 End</option>
+            <option value="12:30:00">12:30 End</option>
+            <option value="13:00:00">13:00 End</option>
+            <option value="13:30:00">13:30 End</option>
+            <option value="14:00:00">14:00 End</option>
+            <option value="14:30:00">14:30 End</option>
+            <option value="15:00:00">15:00 End</option>
+            <option value="15:30:00">15:30 End</option>
+            <option value="16:00:00">16:00 End</option>
+            <option value="16:30:00">16:30 End</option>
+            <option value="17:00:00">17:00 End</option>
           </select>
         </label>
 
--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -9,9 +9,7 @@ import "./mainContent.css";
 export default function MainContent() {
   return (
     <section className="main-display">
-      <h1>Clan Optimist's Event App</h1>
-
-      <h2>Upcoming events</h2>
+      <h2>Clan Optimist's upcoming...</h2>
       <div id="upcoming-event">
         <DisplayUpcomingEvents count="1" />
       </div>
@@ -20,10 +18,14 @@ export default function MainContent() {
       <section className="calendar-placement">
         <MyCalendar />
       </section>
-      <p>
+      {/* <p>
         Bootcamp is no walk in the park. So let’s make it a little easier for
         you. Want to create an event to get together with your fellow
         bootcampers? Forgot when code club is? Let's help you!
+      </p> */}
+      <p>
+        Optimist's Flexible, Social and Professional event manager. Never worry
+        about missing a deadline.{" "}
       </p>
     </section>
   );
