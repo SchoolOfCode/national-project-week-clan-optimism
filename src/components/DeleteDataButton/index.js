878ec37cc8ac393c989388b5b51993c7417595c3
EzyhB <ezyhbmail@gmail.com>
2022-01-20 09:42:08 +0000

    every fetch taken out of useEffect() and put into another function

--- src/components/DeleteDataButton/index.js
+++ src/components/DeleteDataButton/index.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./deleteDataButton.css";
 
 export default function DeleteDataButton({ eventid }) {
-  const [deleteID, setDeleteID] = useState(0);
-
-  useEffect(() => {
-    fetch("http://localhost:5500/api/v1/events/" + deleteID, {
+  function handleOnClick(e) {
+    fetch("http://localhost:5500/api/v1/events/" + e.target.value, {
       method: "DELETE",
     });
-    console.log("delete req passed");
-  }, [deleteID]);
-
-  function handleOnClick(e) {
-    setDeleteID(e.target.value);
-    console.log(e.target.value);
+    console.log("delete req passed").then(console.log("delete done"));
   }
 
   return (
---
+++ src/components/EditDataButton/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function EditDataButton({ eventid }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const editObj = {
+      event_description: e.target[0].value,
+      id: e.target[1].value,
+    };
+
+    fetch("http://localhost:5500/api/v1/events/" + editObj.id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editObj.event_description),
+    });
+  }
+
+  return (
+    <form
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
+      <input placeholder="edit event description..." />
+      <button className="edit-event-button" type="submit" value={eventid}>
+        Edit
+      </button>
+    </form>
+  );
+}
