6725414a8bd3302fc2036af98828903cc593e4d6
thuannguyen88 <getaholdofthuan@gmail.com>
2022-01-20 10:37:44 +0000

    deploy fixes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HostEventPage from "../HostEventPage";
 import HomePage from "../HomePage";
 import DisplayAllEventsPage from "../DisplayAllEventsPage";
